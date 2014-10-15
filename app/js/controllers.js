feb409e7c2d6988d6581fc18a28b75f661dee943
Justin Greco <grecoj@administorsmbp2.nc.rr.com>
2014-10-15 11:18:09 -0400

    added provider and cost information

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,20 +15,51 @@ angular.module('myApp.controllers', [])
   		}
   	});
   	$http.get("data/fiber.geojson").success(function(data, status) {
+
         angular.extend($scope, {
             geojson: {
                 data: data,
-                style: {
+                style: 
+                  function (feature) {return {};},
+                  pointToLayer: function(feature, latlng) {
+                      var iconUrl = "img/marker-green.png",
+                        iconRetinaUrl = "img/marker-green-2x.png";
+                      switch (feature.properties.provider) {
+                          case "Time Warner Cable":
+                            iconUrl = "img/marker-icon.png";
+                            iconRetinaUrl = "img/marker-icon-2x.png";
+                          break;
+                          case "AT&T":
+                            iconUrl = "img/marker-orange.png";
+                            iconRetinaUrl = "img/marker-orange-2x.png";
+                          break;
+                      }
+                      return new L.marker(latlng, {icon: L.icon({iconUrl:iconUrl, iconRetina:iconRetinaUrl})});
+                  
                 }
             }
         });
     });
     $scope.$on('leafletDirectiveMap.geojsonMouseover', function (e, leafletEvent) {
-
+      var properties = leafletEvent.target.feature.properties,
+        provider = "img/raleigh.png";
+      switch (properties.provider) {
+        case "Time Warner Cable":
+          provider = "img/twc.png";
+        break;
+        case "AT&T":
+          provider = "img/att.png";
+        break;
+      }
     	angular.extend($scope, {
-			properties: leafletEvent.target.feature.properties,
-			proposed: leafletEvent.target.feature.properties.new_serv,
-			current: leafletEvent.target.feature.properties.speed
+      provider: provider,
+			site: properties.site,
+			proposed_dl: properties.dl_fut,
+			current_dl: properties.dl_curr,
+      proposed_ul: properties.ul_fut,
+      current_ul: properties.ul_curr,
+      currentCost: properties.cost_curr,
+      futureCost: properties.cost_fut
     	});
     	$scope.callDrawsvg = function (speed, path) {
     		drawsvg(speed, path);
@@ -41,8 +72,10 @@ angular.module('myApp.controllers', [])
     	// }, 1);
 
 
-    	$scope.callDrawsvg(leafletEvent.target.feature.properties.new_serv, document.querySelector('.proposed-line path'));
-    	$scope.callDrawsvg(leafletEvent.target.feature.properties.speed, document.querySelector('.current-line path'));
+    	$scope.callDrawsvg(properties.dl_fut, document.querySelector('.proposed-dl-line path'));
+    	$scope.callDrawsvg(properties.dl_curr, document.querySelector('.current-dl-line path'));
+      $scope.callDrawsvg(properties.ul_fut, document.querySelector('.proposed-ul-line path'));
+      $scope.callDrawsvg(properties.ul_curr, document.querySelector('.current-ul-line path'));
 
     });
 
