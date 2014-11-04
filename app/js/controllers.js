369cd959e72298e461d4fbd1fcdb3b99b2717ed2
Justin Greco <grecoj@administorsmbp2.ci.raleigh.nc.us>
2014-11-04 15:17:37 -0500

    updated layout

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,13 +6,15 @@ angular.module('myApp.controllers', [])
   .controller('MapController', ['$scope', '$http', 'leafletData', 'drawsvg', function($scope, $http, leafletData, drawsvg) {
     angular.extend($scope, {
       raleigh: {
-        lat: 35.83,
+        lat: 35.79,
         lng: -78.6436,
-        zoom: 11
+        zoom: 10
       },
       tiles: {
         url: 'http://a.tiles.mapbox.com/v3/examples.3hqcl3di/{z}/{x}/{y}.png'
-      }
+      },
+      showInfo: false,
+      sites: []
     });
     $http.get("data/fiber.geojson").success(function(data, status) {
 
@@ -34,6 +36,7 @@ angular.module('myApp.controllers', [])
                             iconRetinaUrl = "img/marker-orange-2x.png";
                           break;
                       }
+                      $scope.sites.push(feature.properties.site);
                       return new L.marker(latlng, {icon: L.icon({iconUrl:iconUrl, iconRetina:iconRetinaUrl})});
                   
                 }
@@ -41,7 +44,15 @@ angular.module('myApp.controllers', [])
         });
     });
 
+    $scope.siteSelected = function () {
+      $scope.updateSiteInfo($scope.selectedSite);
+      leafletData.getMap().then(function(map) {
+          map.setView($scope.selectedSite.geometry.coordinates.reverse(), 16);
+      });
+    };
+
     $scope.updateSiteInfo = function (feature) {
+      $scope.selectedSite = feature;
      var properties = feature.properties,
         provider = "img/raleigh.png";
       switch (properties.provider) {
@@ -54,13 +65,11 @@ angular.module('myApp.controllers', [])
       }
       angular.extend($scope, {
       provider: provider,
-      site: properties.site,
-      proposed_dl: properties.dl_fut,
-      current_dl: properties.dl_curr,
-      proposed_ul: properties.ul_fut,
-      current_ul: properties.ul_curr,
-      currentCost: properties.cost_curr,
-      futureCost: properties.cost_fut
+      raleighCost: 270,
+      raleigh_ul: 1000,
+      raleigh_dl: 1000,
+      raleighSrc: "img/raleigh.png",
+      showInfo: true
       });
       $scope.callDrawsvg = function (speed, path) {
         drawsvg(speed, path);
@@ -69,6 +78,8 @@ angular.module('myApp.controllers', [])
       $scope.callDrawsvg(properties.dl_curr, document.querySelector('.current-dl-line path'));
       $scope.callDrawsvg(properties.ul_fut, document.querySelector('.proposed-ul-line path'));
       $scope.callDrawsvg(properties.ul_curr, document.querySelector('.current-ul-line path'));
+      $scope.callDrawsvg(1000, document.querySelector('.raleigh-dl-line path'));
+      $scope.callDrawsvg(1000, document.querySelector('.raleigh-ul-line path'));
     };
 
     $scope.$on('leafletDirectiveMap.geojsonMouseover', function (ev, leafletEvent) {
--- app/js/directives.js
+++ app/js/directives.js
@@ -3,7 +3,7 @@
 /* Directives */
 
 
-angular.module('myApp.directives', ['leaflet-directive', 'angular-dimple']).
+angular.module('myApp.directives', ['leaflet-directive']).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
